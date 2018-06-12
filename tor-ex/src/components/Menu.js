560b8ee22102f6fe26606af4b6a8bb089ea0a31f
seongjun lee <rooneylee@outlook.com>
2018-06-11 18:38:28 -0700

    POST Magnet Link

--- tor-ex/src/components/Menu.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default class Menu extends React.Component {
-  render() {
-    // need to add icons
-    return <button onClick={this.props.onClick}>{this.props.name}</button>;
-  }
-}
--- tor-ex/src/components/MenuBar.js
+++ tor-ex/src/components/MenuBar.js
@@ -1,33 +1,76 @@
 import React from "react";
-import Menu from "./Menu.js";
+import axios from "axios";
+import Popup from "reactjs-popup";
 
 export default class MenuBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      menus: ["Magnet", "Seed", "Setting"]
+      magnetLink: ""
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChangeMagnet = this.handleChangeMagnet.bind(this);
+    this.handleSubmitMagnet = this.handleSubmitMagnet.bind(this);
   }
 
-  handleClick(menu) {
-    // TODO
-    // 0. vim error detaction fix(ale)
-    // 1. axios implementation
-    // 2. react popup
-    // 3. props icon
-    // 4. alignment of menus
-    console.log(menu);
+  handleChangeMagnet(event) {
+    this.setState({ magnetLink: event.target.value });
   }
+
+  handleSubmitMagnet(event) {
+    event.preventDefault();
+
+    const link = {
+      link: this.state.magnetLink
+    };
+
+    axios
+      .post(`https://httpbin.org/post`, { link })
+      .then(res => {
+        console.log("Post[Magnet] Success");
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log("Post[Magnet] Failed");
+        console.log(err);
+      });
+  }
+
+  // TODO
+  // 0. vim error detaction fix(ale)
+  // 1. axios implementation
+  // 2. react popup
+  // 3. props icon
+  // 4. alignment of menus
 
   render() {
-    const deployMenu = menus => {
-      return menus.map((menu, i) => {
-        return (
-          <Menu onClick={() => this.handleClick(menu)} name={menu} key={i} />
-        );
-      });
-    };
-    return <div>{deployMenu(this.state.menus)}</div>;
+    return (
+      <div>
+        <Popup trigger={<button className="button"> Magnet </button>}>
+          {close => (
+            <form
+              onSubmit={e => {
+                this.handleSubmitMagnet(e);
+                close();
+              }}
+            >
+              <label>
+                Person Name:
+                <input type="text" name="name" onChange={this.handleChange} />
+              </label>
+              <button type="submit">Add</button>
+            </form>
+          )}
+        </Popup>
+
+        <Popup trigger={<button className="button"> Seed </button>}>
+          <span> Seed content </span>
+        </Popup>
+
+        <Popup trigger={<button className="button"> Setting </button>}>
+          <span> Setting content </span>
+        </Popup>
+      </div>
+    );
   }
 }
